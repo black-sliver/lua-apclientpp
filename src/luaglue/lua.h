7d928006f994fcfe4e0b195145a61bd1c3a7dd22
black-sliver <59490463+black-sliver@users.noreply.github.com>
2023-04-20 02:15:53 +0200

    fix lua push overload

--- src/luaglue/lua.h
+++ src/luaglue/lua.h
@@ -38,20 +38,20 @@ public:
     {
         lua_pushstring(L, s.c_str());
     }
-    void Push(lua_Integer i)
-    {
-        lua_pushinteger(L, i);
-    }
     void Push(lua_Number n)
     {
         lua_pushnumber(L, n);
     }
+    void Push(int32_t i)
+    {
+        lua_pushinteger(L, i);
+    }
     void Push(int64_t n)
     {
         if (n < std::numeric_limits<int64_t>::min() || n > std::numeric_limits<int64_t>::max())
-            Push((lua_Number)n);
+            lua_pushnumber(L, (lua_Number)n);
         else
-            Push((lua_Integer)n);
+            lua_pushinteger(L, (lua_Integer)n);
     }
     void Push(bool b)
     {
